526d0b3d2e393dedc25a1eebb6268ab5f5f8571b
Diogo Santos <diogo@diogosantos.com>
2011-07-07 19:16:28 -0300

    adding production configuration

--- clickspot.rb
+++ clickspot.rb
@@ -6,10 +6,15 @@ configure :development do
   MongoMapper.database = 'clickspot'
 end
 
+configure :production do
+  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10010)
+  MongoMapper.database = 'clickspot'
+  MongoMapper.database.authenticate(ENV['clickspot'], ENV['clickspot'])
+end
+
 get "/" do
   "ok"
 end
-
 
 post '/click' do
   click = Click.new(params)
